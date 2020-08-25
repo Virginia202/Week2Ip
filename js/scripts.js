8ab343c27fecd750062fad8189c61ba7ec973211
Virginia202 <virgisonie20@gmail.com>
2020-08-25 06:51:15 +0300

    add js

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function testResults(){
+    var TestVar = document.getElementById("fname").Value;
+        var
+        Dy=document.getElementById("ddate").value;
+        var
+        gender=document.getElementByName("gender");
+        document.getElementById("name").innerHTML=TestVar;
+        document.getElementById("date").innerHTML=Dy;
+
+        var myGender="male";
+        for(var i=0,length=gender.length; i<length;i++){
+            if(gender[i].checked){
+                myGender=gender[i].value;
+                document.getElementById("genderLabel").innerHTML=myGender;
+                break;
+            }
+        }
+
+        var mydate = newDate(Dy);
+        var datOfWeek=mydate.getDay();
+        var malesArray=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+        var femaleArray= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+        var myName="";
+        if(myGender)
+}
